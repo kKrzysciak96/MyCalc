99a451865fc5c502f67714d66065d84a6b3f6e64
krzys <krzysztof.krzysciak@adres.pl>
2023-10-18 11:43:07 +0200

    Material3 changes.

---
+++ app/src/main/java/com/eltescode/mycalc/presentation/CalculatorUiAction.kt
@@ -0,0 +1,19 @@
+package com.eltescode.mycalc.presentation
+
+import androidx.compose.runtime.Composable
+import com.eltescode.mycalc.domain.CalculatorAction
+
+
+data class CalculatorUiAction(
+    val text: String?,
+    val highlightLevel : HighlightLevel,
+    val action: CalculatorAction,
+    val content: @Composable ()->Unit = {}
+)
+
+sealed interface HighlightLevel{
+object Neutral: HighlightLevel
+object SemiHighlighted: HighlightLevel
+object Highlighted: HighlightLevel
+object StronglyHighlighted: HighlightLevel
+}
